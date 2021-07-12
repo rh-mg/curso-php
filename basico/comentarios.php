8b13cf76896474ce0d47f6cd1054fc3de7e4b1f6
rh-mg <rhamon.mg@gmail.com>
2021-07-12 18:56:25 -0300

    Integração php html css realizada.

---
+++ basico/comentarios.php
@@ -0,0 +1,28 @@
+<div class="titulo">Comentários PHP</div>
+
+<?php
+echo "Estou no PHP";
+// echo "comentário";
+# echo "comentário";
+?>
+
+<p> Depois do primeiro bloco!</p>
+
+<!-- 
+    <p>Comentário HTML</p>
+    <p>Comentário HTML</p>
+    <p>Comentário HTML</p>
+-->
+
+<?php
+/*
+    Linha 1 do comentário
+    Linha 2 do comentário
+    Linha 3 do comentário
+
+ */
+?> 
+
+echo "<p>Depois do segundo bloco!</p>"
+
+
